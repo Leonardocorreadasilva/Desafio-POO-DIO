97b4e7962fe38358c6d7410454759856b5cd0e81
Leonardo <97353984+Leonardocorreadasilva@users.noreply.github.com>
2022-11-27 15:16:57 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("SmartPhone Nokia: ");
+Smartphone nokia = new Nokia(numero: "1264597", modelo: "Nokia One", imei: "132465789", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Candy crush");
+
+Console.WriteLine();
+
+Console.WriteLine("Vez do iphone");
+
+Smartphone iphone = new Iphone(numero: "1265425", modelo: "Iphone14", imei: "1234", memoria: 258);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("APP de Banco");
